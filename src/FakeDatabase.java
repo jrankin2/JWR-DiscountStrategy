0b74e008caf93a41d13add4d380c30dc3efceafb
jrankin2 <jrankin2@L11706038602.wctc.msft>
2013-03-12 16:33:34 -0500

    changed db code to 1 line getters

--- src/FakeDatabase.java
+++ src/FakeDatabase.java
@@ -13,19 +13,6 @@ public class FakeDatabase implements InformationStrategy{
     private Map<String, Customer> customers = new HashMap<String, Customer>();
     private Map<String, Product> products = new HashMap<String, Product>();
     
-    /*
-    private Customer[] customers = {
-        new Customer("C100", "John", "Smith"),
-        new Customer("C200", "Sally", "Jones"),
-        new Customer("C300", "Bob", "Clementi")
-    };
-    
-    private Product[] products = {
-        new Product("A101", "Baseball Hat", 19.95, new VariableRateDiscount(0.15)),
-        new Product("A102", "Men's Dress Shirt", 35.50, new QuantityDiscount(.10, 5)),
-        new Product("A103", "Women's Socks", 9.50, new NoDiscount())
-    };
-    */
     public FakeDatabase(){
         //populate the "databases"...
         customers.put("C100", new Customer("C100", "John", "Smith"));
@@ -45,23 +32,7 @@ public class FakeDatabase implements InformationStrategy{
             throw new IllegalArgumentException();
         }
         
-        if(!customers.containsKey(custId)){
-            return null;
-        } else{
-            return customers.get(custId);
-        }
-        
-        /*
-        Customer customer = null;
-        for (Customer c : customers) {
-            if (custId.equals(c.getId())) {
-                customer = c;
-                break;
-            }
-        }
-
-        return customer;
-        */
+        return (!customers.containsKey(custId) ? customers.get(custId) : null );
     }
 
     // Find a product in the array by its id.
@@ -71,22 +42,6 @@ public class FakeDatabase implements InformationStrategy{
             throw new IllegalArgumentException();
         }
         
-        if(!products.containsKey(prodId)){
-            return null;
-        } else{
-            return products.get(prodId);
-        }
-        
-        /*
-        Product product = null;
-        for (Product p : products) {
-            if (prodId.equals(p.getId())) {
-                product = p;
-                break;
-            }
-        }
-
-        return product;
-        */
+        return (products.containsKey(prodId) ? products.get(prodId) : null);
     }
 }
--- src/InformationStrategy.java
+++ src/InformationStrategy.java
@@ -6,7 +6,6 @@
 public interface InformationStrategy extends CustomerInformationStrategy, ProductInformationStrategy{
     
     //public abstract Product findProduct(String productId);
-    
     //public abstract Customer findCustomer(String customerId);
     
 }
